16a264fb4d320387eeb51849d04902d93c704365
Antoine Chagnon Michaud <achagnonmichaud@cvm.qc.ca>
2024-09-09 16:21:32 -0400

    ready

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,21 @@ import TheHeader from './component/TheHeader'
 import AFindTheNumberGame from './component/AFindTheNumberGame'
 import TheCanva from './component/TheCanvas'
 import TheForm from './component/TheForm'
-import { ChangeEvent, useState } from 'react'
 
 const App = () => {
-
-  const [cssClass, setCssClass] = useState<string>('ligth-mode')
-  function handleToggle(event: ChangeEvent<HTMLInputElement>): void {
-    setCssClass(event.target.checked ? 'dark-mode' : 'ligth-mode');
-  }
-
+  const cssClass = 'ligth-mode'
+  // va lire sur useState 
   return (
     <div className={cssClass}>
       <TheHeader />
       <div className='wrapper'>
-        <input type="checkbox" onChange={handleToggle} />
+        <input type="checkbox" />
         <h1>Plan de match!</h1>
         <p>Comprendre comment prévenir le comportement d'un event</p>
         <p>Comprendre comment intéragir avec l'utilisateur à partir d'un event</p>
         <p>BONUS : afficher des affaires avec un event</p>
+        <h2>En fin de cours</h2>
+        <p>useState</p>
         <TheForm />
         <AFindTheNumberGame />
         {/* <TheCanva /> */}
